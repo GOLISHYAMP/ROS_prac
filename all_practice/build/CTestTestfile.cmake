8e1f69a585a5c3a8e39749624a3c8500a9503db5
Shyam Goli <golishyamgolisai@gmail.com>
2023-04-14 21:17:18 +0530

    initial commit of all_practice folder

---
+++ all_practice/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shyam/ROS_practice/all_practice/src
+# Build directory: /home/shyam/ROS_practice/all_practice/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot_recollect")
