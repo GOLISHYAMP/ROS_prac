0a696691f0b0495c559dea8476e15742018d49c7
Shyam Goli <golishyamgolisai@gmail.com>
2023-04-16 13:39:54 +0530

    made a asynchronous client

--- actions/catkin_ws/src/my_robot_tutorials/scripts/count_until_client.py
+++ actions/catkin_ws/src/my_robot_tutorials/scripts/count_until_client.py
@@ -15,13 +15,21 @@ class CountUntilClient:
         
     def send_goal_and_get_result(self):
         goal = CountUntilGoal(max_number = 10, wait_duration = 0.5)
-        self._ac.send_goal(goal)
+        self._ac.send_goal(goal,done_cb = self.done_callback)
         rospy.loginfo("Goal has been sent")
-        self._ac.wait_for_result()
-        rospy.loginfo(self._ac.get_result())
+        #self._ac.wait_for_result()
+        #rospy.loginfo(self._ac.get_result())
+        count = 0
+        while not (count == 10):
+            print(count)
+            count += 1
+
+    def done_callback(self, status, result):
+        rospy.loginfo("status is : "+str(status))
+        rospy.loginfo("Result is : "+str(result))
 
 if __name__ == '__main__':
     rospy.init_node("count_until_client")
     client = CountUntilClient()
     client.send_goal_and_get_result()
-
+    rospy.spin()
