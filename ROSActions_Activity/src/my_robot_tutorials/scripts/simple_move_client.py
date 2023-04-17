898fbf9707659e1cf6bd41e4b297deba58fe07a3
Shyam Goli <golishyamgolisai@gmail.com>
2023-04-18 00:26:07 +0530

    completed activity

--- ROSActions_Activity/src/my_robot_tutorials/scripts/simple_move_client.py
+++ ROSActions_Activity/src/my_robot_tutorials/scripts/simple_move_client.py
@@ -5,3 +5,29 @@ import actionlib
 
 from my_robot_msgs.msg import moveAction
 from my_robot_msgs.msg import moveResult
+from my_robot_msgs.msg import moveGoal
+
+class simple_move_client():
+    def __init__(self):
+        self._ac = actionlib.SimpleActionClient("/moverobot",moveAction)
+        self._ac.wait_for_server()
+        rospy.loginfo("Action Server is up and ready to receive goals")
+        
+    def send_goal_and_get_results(self):
+        goal = moveGoal(position = 86, velocity = 5)
+        self._ac.send_goal(goal,done_cb=self.done_callback,feedback_cb=self.feedback_callback)
+        rospy.loginfo("Goal has been sent")
+
+    def done_callback(self,status,result):
+        rospy.loginfo("Status is : "+str(status))
+        rospy.loginfo("Result is : "+str(result))
+
+    def feedback_callback(self,feedback):
+        rospy.loginfo("Feedback message is : "+str(feedback.message))
+        rospy.loginfo("Current position is : "+str(feedback.position))
+
+if __name__ == "__main__":
+    rospy.init_node("simple_move_client")
+    rospy.loginfo("Simple move client has been started")
+    smc = simple_move_client()
+    smc
--- ROSActions_Activity/src/my_robot_tutorials/scripts/simple_move_server.py
+++ ROSActions_Activity/src/my_robot_tutorials/scripts/simple_move_server.py
@@ -5,12 +5,46 @@ import actionlib
 
 from my_robot_msgs.msg import moveAction
 from my_robot_msgs.msg import moveResult
-from my_robot_msgs.msg import moveGoal
-#from my_robot_msgs.msg import moveAction
+# from my_robot_msgs.msg import moveGoal
+from my_robot_msgs.msg import moveFeedback
 
 class simple_move_server():
-    def __init__():
-        server = actionlib.SimpleActionServer("/moverobot",moveAction,execute_cb = on_goal,auto_start = False) 
+    def __init__(self):
+        self._as = actionlib.SimpleActionServer("/moverobot",moveAction,execute_cb = self.on_goal,auto_start = False)
+        self._as.start()
+        rospy.loginfo("Simple Action Server has been started")
+        self.current_position = 50
+
+    def on_goal(self,goal): 
+        rospy.loginfo("Goal has been received")
+        self.position = goal.position
+        self.velocity = goal.velocity
+        rate = rospy.Rate(1/self.velocity)
+        feedback = moveFeedback()
+        if self.position < self.current_position:
+            while self.position < self.current_position:
+                self.current_position -= 1
+                feedback.message = "moving to left"
+                feedback.position = self.current_position
+                self._as.publish_feedback(feedback)
+                rate.sleep()
+        elif self.position > self.current_position:
+            while self.position > self.current_position:
+                self.current_position += 1
+                feedback.message = "moving to Right"
+                feedback.position = self.current_position
+                self._as.publish_feedback(feedback)
+                rate.sleep()
+        else:
+            feedback.message = "Current and Goal positions are same"
+            feedback.position = self.current_position
+            self._as.publish_feedback(feedback)
+        result = moveResult()
+        result.current_position = self.current_position
+        rospy.loginfo("Result has been sent from server to client")
+        self._as.set_succeeded(result)
+
+        
 
 
 
@@ -19,4 +53,4 @@ if __name__ == "__main__":
     rospy.init_node("Simple_server_node")
     rospy.loginfo("Simple server node has been started")
     sms = simple_move_server()
-
+    rospy.spin()
