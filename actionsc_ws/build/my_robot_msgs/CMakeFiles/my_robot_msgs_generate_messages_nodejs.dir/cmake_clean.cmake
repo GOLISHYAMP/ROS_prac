6c3f4c20e0ff4ebc867b7cecb3465b74de6ee1ed
Shyam Goli <golishyamgolisai@gmail.com>
2023-04-22 21:53:39 +0530

    initial commit of action server and client

---
+++ actionsc_ws/build/my_robot_msgs/CMakeFiles/my_robot_msgs_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/home/shyam/ROS_practice/actionsc_ws/devel/share/gennodejs/ros/my_robot_msgs/msg/CountUntilAction.js"
+  "/home/shyam/ROS_practice/actionsc_ws/devel/share/gennodejs/ros/my_robot_msgs/msg/CountUntilActionFeedback.js"
+  "/home/shyam/ROS_practice/actionsc_ws/devel/share/gennodejs/ros/my_robot_msgs/msg/CountUntilActionGoal.js"
+  "/home/shyam/ROS_practice/actionsc_ws/devel/share/gennodejs/ros/my_robot_msgs/msg/CountUntilActionResult.js"
+  "/home/shyam/ROS_practice/actionsc_ws/devel/share/gennodejs/ros/my_robot_msgs/msg/CountUntilFeedback.js"
+  "/home/shyam/ROS_practice/actionsc_ws/devel/share/gennodejs/ros/my_robot_msgs/msg/CountUntilGoal.js"
+  "/home/shyam/ROS_practice/actionsc_ws/devel/share/gennodejs/ros/my_robot_msgs/msg/CountUntilResult.js"
+  "CMakeFiles/my_robot_msgs_generate_messages_nodejs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/my_robot_msgs_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
