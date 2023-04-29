33e35894cb9ed2b4ab9e769a05df129bd6773e7a
Shyam Goli <golishyamgolisai@gmail.com>
2023-04-29 22:34:30 +0530

    changes in the action_client and server

--- actionsc_ws/src/my_robot_codes/scripts/action_client.py
+++ actionsc_ws/src/my_robot_codes/scripts/action_client.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import rospy
 import actionlib
@@ -7,21 +7,55 @@ from my_robot_msgs.msg import CountUntilAction
 from my_robot_msgs.msg import CountUntilGoal
 
 class CountUntilClient:
-
+   
     def __init__(self):
-        self._ac = actionlib.ActionClient("/count_until",CountUntilAction)
+        self._ac = actionlib.ActionClient('/count_until',
+            CountUntilAction)
         self._ac.wait_for_server()
-        rospy.loginfo("Action server is up and ready to receive goals")
+        rospy.loginfo("Action server up, we can send goals.")
+        # self._goal_handles = {}
 
-    def send_goal(self,max_number, wait_duration)
+    def on_transition(self, goal_handle):
+        # index = 0
+        # for i in self._goal_handles:
+        #     if self._goal_handles[i] == goal_handle:
+        #         index = i
+        #         break
+        
+        # rospy.loginfo(str(index) + " --- Transition callback")
+        #rospy.loginfo(goal_handle.get_comm_state())
+        #rospy.loginfo(goal_handle.get_goal_status())
+
+        if goal_handle.get_comm_state() == 2:
+            rospy.loginfo(str(goal_handle.get_goal_id()) + ": Goal just went active.")
+        if goal_handle.get_comm_state() == 7:
+            rospy.loginfo(str(goal_handle.get_goal_id()) + ": Goal is DONE")
+            rospy.loginfo(goal_handle.get_terminal_state())
+            rospy.loginfo(goal_handle.get_result())
+
+    def on_feedback(self, goal_handle, feedback):
+        #rospy.loginfo("--- Feedback callback")
+        #rospy.loginfo(feedback)
+        pass
+
+    def send_goal(self, max_number, wait_duration):
         goal = CountUntilGoal()
         goal.max_number = max_number
         goal.wait_duration = wait_duration
-        rospy.loginfo("goal send")
-        goal_handle = self._ac.send_goal
+        rospy.loginfo("Send goal.")
+        goal_handle = self._ac.send_goal(goal,self.on_transition,self.on_feedback)
+        return goal_handle
 
-if __name__ == "__main__":
-    rospy.init_node("count_until_client")
+if __name__ == '__main__':
+    rospy.init_node('count_until_client')
     client = CountUntilClient()
-    client.send_goal(8,0.5)
+    client.send_goal(15, 0.5)
+    # goal_handle1 = client.send_goal(15, 0.5)
+    # client._goal_handles['1'] = goal_handle1
+    # goal_handle2 = client.send_goal(20, 0.8)
+    # client._goal_handles['2'] = goal_handle2
+
+    # rospy.sleep(1.0)
+    # goal_handle1.cancel()
+
     rospy.spin()
--- actionsc_ws/src/my_robot_codes/scripts/action_server.py
+++ actionsc_ws/src/my_robot_codes/scripts/action_server.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
+import threading
 import rospy
 import actionlib
-import threading
 
 from my_robot_msgs.msg import CountUntilAction
 from my_robot_msgs.msg import CountUntilGoal
@@ -10,20 +10,24 @@ from my_robot_msgs.msg import CountUntilResult
 from my_robot_msgs.msg import CountUntilFeedback
 
 class CountUntilServer:
+
     def __init__(self):
-        self._as = actionlib.ActionServer('/count_until',CountUntilAction,self.on_goal,self.on_cancel,auto_start = False)
+        self._as = actionlib.ActionServer('/count_until',
+            CountUntilAction, self.on_goal, self.on_cancel,
+            auto_start=False)
         self._as.start()
+        rospy.loginfo("Action Server has been started.")
+        # self._cancel_goals = {}
 
-        rospy.loginfo("Action Server has been started!")
-        self._cancel_request = False
-
-    def process_goal(self,goal_handle):
-        rospy.loginfo("Processing goal")
+    def process_goal(self, goal_handle):
+        goal_id = goal_handle.get_goal_id()
+        rospy.loginfo("Processing goal : " + str(goal_id.id))
         goal = goal_handle.get_goal()
         max_number = goal.max_number
         wait_duration = goal.wait_duration
 
-        if max_number>10:
+        # Validate parameters
+        if max_number > 10:
             goal_handle.set_rejected()
             return
         goal_handle.set_accepted()
@@ -35,23 +39,23 @@ class CountUntilServer:
         preempted = False
 
         while not rospy.is_shutdown():
-            
-            if self._cancel_request:
+            counter += 1
+            rospy.loginfo(str(goal_id.id) + ":  " + str(counter))
+            if self._cancel_goals[goal_id]:
                 preempted = True
                 break
             if counter >= max_number:
                 success = True
                 break
-            counter += 1
-            rospy.loginfo("Counter : "+str(counter))
             feedback = CountUntilFeedback()
-            feedback.percentage = float(counter)/float(max_number)
+            feedback.percentage = \
+                float(counter) / float(max_number)
             goal_handle.publish_feedback(feedback)
             rate.sleep()
 
         result = CountUntilResult()
         result.count = counter
-        rospy.loginfo("send goal result to client")
+        rospy.loginfo("Send goal result to client")
 
         if preempted:
             rospy.loginfo("Preempted")
@@ -62,23 +66,42 @@ class CountUntilServer:
         else:
             rospy.loginfo("Aborted")
             goal_handle.set_aborted(result)
-        rospy.loginfo("Finished the processing of the goal")
-            
+        rospy.loginfo("--- Finished to process the goal : " + \
+            str(goal_id.id))
+        # self._cancel_goals.pop(goal_id)
 
-    def on_goal(self,goal_handle):
+    def on_goal(self, goal_handle):
         rospy.loginfo("Received new goal")
         rospy.loginfo(goal_handle.get_goal())
-        w = threading.Thread(name="worker", target=self.process_goal,args = (goal_handle,))
+
+        # change goal policy -> only one goal active
+        #if len(self._cancel_goals) != 0:
+        #    rospy.logwarn("A goal already exists! Reject new goal")
+        #    result = CountUntilResult()
+        #    result.count = -1000
+        #    goal_handle.set_rejected(result)
+        #    return
+
+        # self._cancel_goals[goal_handle.get_goal_id()] = False
+        # rospy.loginfo("List of goals")
+        # rospy.loginfo(self._cancel_goals)
+
+        w = threading.Thread(name="worker", 
+            target=self.process_goal, args=(goal_handle,))
         w.start()
+    
+    def on_cancel(self, goal_handle):
+        rospy.loginfo("Received cancel request.")
+        goal_id = goal_handle.get_goal_id()
+        rospy.loginfo(str(goal_id)+" goal is cancelled")
+        # if goal_id in self._cancel_goals:
+        #     rospy.loginfo("Found goal to cancel")
+        #     self._cancel_goals[goal_id] = True
 
-    def on_cancel(self,goal_handle):
-        rospy.loginfo("Requested for cancel received")
-        self._cancel_request = True
 
-if __name__ == "__main__":
-    rospy.init_node("count_until_server")
+if __name__ == '__main__':
+    rospy.init_node('count_until_server')
 
     server = CountUntilServer()
 
     rospy.spin()
-
